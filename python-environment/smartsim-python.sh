f5f99d30dc3d6b94f799a434ce5967f60c992923
boss507104 <boss507104@naver.com>
2026-07-31 15:02:58 +0300

    Update SmartSim-CSC revision

--- python-environment/smartsim-python.sh
+++ python-environment/smartsim-python.sh
@@ -23,7 +23,7 @@ set -Eeuo pipefail
 # USER CONFIGURATION
 # ================================================================
 readonly SMARTSIM_CSC_REPO="https://github.com/PentagonToy/SmartSim-CSC.git"
-readonly SMARTSIM_CSC_REF="f68e02c1934400350b23663d26bbfba3be706074"
+readonly SMARTSIM_CSC_REF="bb7c56b80ae56ce654fe6c8b8a08152626dce431"
 
 readonly X64_GCC_MODULE="gcc/13.4.0"
 readonly X64_CMAKE_MODULE="cmake/3.26.5"
